c154ec4151f4cf2ff910c943558cef30870ad4cf
Chris Beer <cabeer@stanford.edu>
2015-02-22 19:49:54 -0800

    fix up tests

--- spec/lib/solr_wrapper_spec.rb
+++ spec/lib/solr_wrapper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe SolrWrapper do
   describe ".wrap" do
     it "should launch solr" do
-      SolrWrapper.wrap(download_path: './tmp') do |solr|
+      SolrWrapper.wrap do |solr|
         expect(Timeout::timeout(15) do
           begin
             s = TCPSocket.new('127.0.0.1', solr.port)
