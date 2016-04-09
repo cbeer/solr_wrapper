1b639aac4d32c4fc1d27d25fdbbdeef9c2ea89c7
Chris Beer <cabeer@stanford.edu>
2016-04-08 19:10:34 -0700

    Provide a 'safe' progressbar that doesn't raise exceptions on bad data; fixes #25

--- lib/solr_wrapper/downloader.rb
+++ lib/solr_wrapper/downloader.rb
@@ -1,16 +1,22 @@
+require 'ruby-progressbar'
+
 module SolrWrapper
   class Downloader
     def self.fetch_with_progressbar(url, output)
-      pbar = ProgressBar.create(title: File.basename(url), total: nil, format: "%t: |%B| %p%% (%e )")
-      open(url, content_length_proc: lambda do|t|
-        if t && 0 < t
-          pbar.total = t
-        end
-      end,
-                progress_proc: lambda do|s|
-                  pbar.progress = s
-                end) do |io|
+      pbar = SafeProgressBar.new(title: File.basename(url), total: nil, format: '%t: |%B| %p%% (%e )')
+      open(url, content_length_proc: ->(bytes) { pbar.total = bytes }, progress_proc: ->(bytes) { pbar.progress = bytes }) do |io|
         IO.copy_stream(io, output)
+      end
+    end
+
+    class SafeProgressBar < ProgressBar::Base
+      def progress=(new_progress)
+        self.total = new_progress if total <= new_progress
+        super
+      end
+
+      def total=(new_total)
+        super if new_total && new_total > 0
       end
     end
   end
