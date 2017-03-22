98996d00c61f7822f7027e5fcf700ea04aaad88d
Justin Coyne <jcoyne@justincoyne.com>
2017-03-22 09:44:10 -0500

    Use webmock instead of fakeweb
    Fakeweb isn't being developed any longer and doesn't work on newer
    rubies

--- spec/lib/solr_wrapper/client_spec.rb
+++ spec/lib/solr_wrapper/client_spec.rb
@@ -1,25 +1,43 @@
 require 'spec_helper'
 
-describe SolrWrapper::Client do
+RSpec.describe SolrWrapper::Client do
   subject { described_class.new('http://localhost:8983/solr/') }
 
   describe '#exists?' do
-    it 'checks if a solrcloud collection exists' do
-      FakeWeb.register_uri(:get, 'http://localhost:8983/solr/admin/collections?action=LIST&wt=json', body: '{ "collections": ["x", "y", "z"]}')
-      FakeWeb.register_uri(:get, 'http://localhost:8983/solr/admin/cores?action=STATUS&wt=json&core=a', body: '{ "status": { "a": {} } }')
-
-      expect(subject.exists?('x')).to eq true
-      expect(subject.exists?('a')).to eq false
+    around do |example|
+      WebMock.disable_net_connect!
+      example.call
+      WebMock.allow_net_connect!
     end
 
-    it 'checks if a solr core exists' do
-      FakeWeb.register_uri(:get, 'http://localhost:8983/solr/admin/collections?action=LIST&wt=json', body: '{ "error": { "msg": "Solr instance is not running in SolrCloud mode."} }')
+    context 'for a solrcloud collection' do
+      before do
+        stub_request(:get, 'http://localhost:8983/solr/admin/collections?action=LIST&wt=json')
+          .to_return(status: 200, body: '{ "collections": ["x", "y", "z"]}')
+        stub_request(:get, 'http://localhost:8983/solr/admin/cores?action=STATUS&wt=json&core=a')
+          .to_return(status: 200, body: '{ "status": { "a": {} } }')
+      end
 
-      FakeWeb.register_uri(:get, 'http://localhost:8983/solr/admin/cores?action=STATUS&wt=json&core=x', body: '{ "status": { "x": { "name": "x" } } }')
-      FakeWeb.register_uri(:get, 'http://localhost:8983/solr/admin/cores?action=STATUS&wt=json&core=a', body: '{ "status": { "a": {} } }')
+      it 'checks if it exists' do
+        expect(subject.exists?('x')).to eq true
+        expect(subject.exists?('a')).to eq false
+      end
+    end
 
-      expect(subject.exists?('x')).to eq true
-      expect(subject.exists?('a')).to eq false
+    context 'for a solr core' do
+      before do
+        stub_request(:get, 'http://localhost:8983/solr/admin/collections?action=LIST&wt=json')
+          .to_return(status: 200, body: '{ "error": { "msg": "Solr instance is not running in SolrCloud mode."} }')
+        stub_request(:get, 'http://localhost:8983/solr/admin/cores?action=STATUS&wt=json&core=x')
+          .to_return(status: 200, body: '{ "status": { "x": { "name": "x" } } }')
+        stub_request(:get, 'http://localhost:8983/solr/admin/cores?action=STATUS&wt=json&core=a')
+          .to_return(status: 200, body: '{ "status": { "a": {} } }')
+      end
+
+      it 'checks if it exists' do
+        expect(subject.exists?('x')).to eq true
+        expect(subject.exists?('a')).to eq false
+      end
     end
   end
 end
